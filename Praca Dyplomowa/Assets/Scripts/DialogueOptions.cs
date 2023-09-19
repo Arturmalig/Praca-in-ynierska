3ca3d8cacb4fbaad99c7521cf9cd07d53c1634d0
DominikFafara <dfafara@student.agh.edu.pl>
2023-09-19 02:03:59 +0200

    Zmiany w skryptach
    Wprowadzenie zmian w obrębie skryptu Obejmującego kliknięcia, stworzenie nowego skryptu, który będzie obsługiwał zmiany tekstu na buttonach w zależności od klikniętej poszlaki, opisanie trochę kodu i oczyszczenie.

---
+++ Praca Dyplomowa/Assets/Scripts/DialogueOptions.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Progress;
+
+public class DialogueOptions : MonoBehaviour
+{
+    public class OptionChoice // zmienna sluzaca do tworzenia nizej tablic tekst-prawda/falsz
+    {
+        public string text { get; set; }
+        public bool value { get; set; }
+    }
+    public OptionChoice[] bloodOp = new OptionChoice[] // tablica zawierajaca opcje na buttony kiedy klikniemy na krew
+    {
+        new OptionChoice { text = "Zbierz", value = true},
+        new OptionChoice { text = "Poliz", value = false},
+        new OptionChoice { text = "Pomaluj", value = false},
+        new OptionChoice { text = "Pogryz", value = false}
+
+    };
+
+    public OptionChoice[] knifeOp = new OptionChoice[] // tablica zawierajaca opcje na buttony kiedy klikniemy na noz
+    {
+        new OptionChoice { text = "Zabezpiecz", value = true},
+        new OptionChoice { text = "Dzgnij", value = false},
+        new OptionChoice { text = "Pomachaj", value = false},
+        new OptionChoice { text = "Rzuc", value = false}
+
+    };
+
+    public bool[] DialReturn(Button op1, Button op2, Button op3, Button op4, string tag) // funkcja zwracajaca odpowiednie napisy na buttonach w zaleznosci od nacisnietego przedmiotu
+    {
+        if (tag == "blood")
+        {
+            op1.GetComponentInChildren<TMP_Text>().text = bloodOp[0].text;
+            op2.GetComponentInChildren<TMP_Text>().text = bloodOp[1].text;
+            op3.GetComponentInChildren<TMP_Text>().text = bloodOp[2].text;
+            op4.GetComponentInChildren<TMP_Text>().text = bloodOp[3].text;
+            bool[] tab1 = new bool[]
+            {
+                bloodOp[0].value, bloodOp[1].value, bloodOp[2].value, bloodOp[3].value
+            };
+            return tab1;
+        }
+        if (tag == "knife")
+        {
+            op1.GetComponentInChildren<TMP_Text>().text = knifeOp[0].text;
+            op2.GetComponentInChildren<TMP_Text>().text = knifeOp[1].text;
+            op3.GetComponentInChildren<TMP_Text>().text = knifeOp[2].text;
+            op4.GetComponentInChildren<TMP_Text>().text = knifeOp[3].text;
+            bool[] tab2 = new bool[]
+{
+                 knifeOp[0].value,  knifeOp[1].value,  knifeOp[2].value,  knifeOp[3].value
+};
+            return tab2;
+        }
+        else return null;
+    }
+
+    public void DialogueClosing(GameObject Panel) // Zamykanie okna dialogowego - zabieg estetyczny, zeby w ObjectClickEvent nie powtarzac 4 razy tych samych linijek
+    {
+        Panel.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
--- Praca Dyplomowa/Assets/Scripts/ObjectClickEvent.cs
+++ Praca Dyplomowa/Assets/Scripts/ObjectClickEvent.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ObjectClickEvent : MonoBehaviour
+public class ObjectClickEvent : DialogueOptions
 {
     // Start is called before the first frame update
 
-    public Camera mainCamera;
-    public float maxClickDistance = 10;
-    public GameObject Panel;
-    public Button Option1;
-    public Button Option2;
-    public Button Option3;
-    public Button Option4;
-    private int Choice=0;
-    private string objectTag;
+    public Camera mainCamera;// Glowna kamera - sluzy do okreslania klikniecia na obiekt
+    public float maxClickDistance = 10;//Maksymalna odleglosc z ktorej mozna kliknac przedmiot
+    public GameObject Panel;// Canvas
+    public Button Option1;// Przycisk 1 na canvas
+    public Button Option2;// Przycisk 2 na canvas
+    public Button Option3;// Przycisk 3 na canvas
+    public Button Option4;// Przycisk 4 na canvas
+    private int Choice=0;// NIE JESTEM PEWIEN JESZCZE TEGO ELEMENTU, MOZE BOOLE ZALATWIA JEGO ZADANIE
+    private string objectTag;//tag obiektu, ktory kliknelismy
+
 
     private void Start()
     {
@@ -24,71 +26,58 @@ public class ObjectClickEvent : MonoBehaviour
         Option3.onClick.AddListener(ChoiceOption3);
         Option4.onClick.AddListener(ChoiceOption4);
         Panel.SetActive(false);
+        
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            bool[] tab = new bool[] { };
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, maxClickDistance))
+            if (Physics.Raycast(ray, out hit, maxClickDistance)) // sprawdzamy czy w dystansie 10 jednostek udalo nam sie w cos kliknac
             {
-                if (hit.collider.gameObject == gameObject && Input.GetMouseButtonDown(0))
+                if (hit.collider.gameObject == gameObject && Input.GetMouseButtonDown(0)) // Sprawdzamy czy nacisnelismy na obiekt lewym przyciskiem myszy
                 {
                     objectTag = gameObject.tag; // Pobieramy tag obiektu, który zosta³ klikniêty
-                    TagChecker(objectTag);
-                    Cursor.visible = true;
-                    Cursor.lockState = CursorLockMode.None;
-                    Panel.SetActive(true);
+                    tab = DialReturn(Option1, Option2, Option3, Option4, objectTag);
+                    Cursor.visible = true; // wlaczamy widocznosc kursora
+                    Cursor.lockState = CursorLockMode.None; // odblokowujemy poruszanie kursorem po ekranie
+                    Panel.SetActive(true); // wlaczamy widocznosc canvas z opcjami wyboru
+                    foreach (bool v in tab) /// FRAGMENT TESTOWY - DO USUNIECIA PAMIETAC
+                    {
+                        Debug.Log(v.ToString() + " "); 
+                    }; /// KONIEC FRAGMENTU TESTOWEGO
                 }
             }
             else Debug.Log("Too far to click");
         }
     }
 
-    public void TagChecker(string tag)
-    {
-        if (tag == "blood")
-        {
-            Debug.Log("Clicked blood");
-        }
-        if (tag == "knife")
-        {
-            Debug.Log("Clicked knife");
-        }
-    }
 
-    public void ChoiceOption1()
+    public void ChoiceOption1() // akcja po wcisnieciu 1 guzika
     {
         Choice = 1;
         Debug.Log("Pressed " + Choice);
-        Panel.SetActive(false);
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        DialogueClosing(Panel);
     }
-    public void ChoiceOption2()
+    public void ChoiceOption2() // akcja po wcisnieciu 2 guzika
     {
         Choice = 2;
         Debug.Log("Pressed " + Choice);
-        Panel.SetActive(false);
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        DialogueClosing(Panel);
     }
-    public void ChoiceOption3()
+    public void ChoiceOption3()// akcja po wcisnieciu 3 guzika
     {
         Choice = 3;
         Debug.Log("Pressed " + Choice);
-        Panel.SetActive(false);
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        DialogueClosing(Panel);
     }
-    public void ChoiceOption4()
+    public void ChoiceOption4()// akcja po wcisnieciu 4 guzika
     {
         Choice = 4;
         Debug.Log("Pressed " + Choice);
-        Panel.SetActive(false);
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        DialogueClosing(Panel);
     }
 }
